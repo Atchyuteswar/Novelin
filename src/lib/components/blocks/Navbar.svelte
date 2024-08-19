d8c589ada575c030505e469701fbbf5e34711b3c
Michael Obubelebra Amachree <amachree9630@gmail.com>
2024-08-19 17:07:14 +0100

    feat(navbar): enhance navbar layout and functionality
    refactor(page): update image import with enhanced query and adjust layout
    
    chore: remove unused book images

--- src/lib/components/blocks/Navbar.svelte
+++ src/lib/components/blocks/Navbar.svelte
@@ -97,27 +97,7 @@
  -->
 
 <nav class="border-gray-200 bg-white dark:bg-gray-900">
-	<div class="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
-		<a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-			<img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-			<span class="self-center whitespace-nowrap text-2xl font-semibold dark:text-white"
-				>Flowbite</span
-			>
-		</a>
-		<button
-			data-collapse-toggle="navbar-default"
-			type="button"
-			class="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
-			aria-controls="navbar-default"
-			aria-expanded="false"
-		>
-			<span class="sr-only">Open main menu</span>
-			{#if navOpen}
-				<X />
-			{:else}
-				<Menu />
-			{/if}
-		</button>
+	<div class="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4 px-14">
 		<div class="hidden w-full md:block md:w-auto" id="navbar-default">
 			<ul
 				class="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 md:dark:bg-gray-900 rtl:space-x-reverse"
@@ -135,5 +115,36 @@
 				{/each}
 			</ul>
 		</div>
+		<!--  -->
+		<a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+			<!-- Change this later -->
+			<img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
+			<span class="self-center whitespace-nowrap text-2xl font-semibold dark:text-white"
+				>Novelin</span
+			>
+		</a>
+		<!--  -->
+		<div class="hidden w-full md:block md:w-auto" id="navbar-default">
+			<ul
+				class="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 md:dark:bg-gray-900 rtl:space-x-reverse"
+			>
+				<li class={`${baseClasses} ${inactiveClasses}`}>Learn More</li>
+			</ul>
+		</div>
+		<!--  -->
+		<button
+			data-collapse-toggle="navbar-default"
+			type="button"
+			class="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
+			aria-controls="navbar-default"
+			aria-expanded="false"
+		>
+			<span class="sr-only">Open main menu</span>
+			{#if navOpen}
+				<X />
+			{:else}
+				<Menu />
+			{/if}
+		</button>
 	</div>
 </nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
 	import { AspectRatio } from '$lib/components/ui/aspect-ratio/index.js';
-	import book1 from '$lib/img/book-5.jpg';
+	import book1 from '$lib/img/book-5.jpg?enhanced';
 </script>
 
-<div class="container mx-auto p-6">
-	<AspectRatio ratio={16 / 9} class="bg-muted">
-		<enhanced:img
-			src={book1}
-			alt="Gray by Drew Beamer"
-			class="h-full w-full rounded-md object-cover"
-		/>
-	</AspectRatio>
-</div>
+<main class="min-h-screen">
+	<div class="container mx-auto p-6">
+		<AspectRatio ratio={16 / 9} class="bg-muted">
+			<enhanced:img
+				src={book1}
+				alt="Books by Jess Bailey"
+				class="h-full w-full rounded-md object-cover"
+			/>
+		</AspectRatio>
+	</div>
 
-<h1>Dive into a World of Stories</h1>
+	<h1 class="text-center text-2xl text-yellow-400 dark:text-yellow-300 md:text-3xl lg:text-5xl">
+		Dive into a World of Stories
+	</h1>
+</main>
